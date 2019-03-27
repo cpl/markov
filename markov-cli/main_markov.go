a3aa3f64c878af5f0d9c979cf4eda65ac97492fb
Alexandru-Paul Copil <alexandru.p.copil@pm.me>
2019-03-27 14:11:44 +0000

    Add markov chain cli

---
+++ markov-cli/main_markov.go
@@ -0,0 +1,34 @@
+package main // import "cpl.li/go/markov/markov-cli"
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+
+	"cpl.li/go/markov"
+)
+
+func main() {
+	// handle flags
+	maxWords := flag.Int("words", 100, "max words to generate (default 100)")
+	pairSize := flag.Int("pairs", 2, "size of a word pair (default 2)")
+	flag.Parse()
+
+	c := markov.NewChain(*pairSize) // create markov chain
+
+	// read stdin
+	data, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// give data as sequence to chain model
+	c.Add(strings.Fields(string(data)))
+
+	b := c.NewBuilder(nil)             // create builder on top of chain
+	b.Generate(*maxWords - c.PairSize) // generate new words
+	fmt.Println(b.String())            // print end product
+}
